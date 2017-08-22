245e2f85ffa915ddde065d396037bfbd94c7f261
Manuel <m.a.n.u.e.l@posteo.net>
2017-08-22 19:52:25 +0200

    Adjusted routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :products
-  resources :sources do
-    get 'list', on: :collection
-  end
+  get 'new_session' => 'sources#list'
+  get 'products' => 'sources#list'
+  get 'recipes' => 'sources#list'
+  get 'components' => 'sources#list'
+  get 'sources' => 'sources#list'
 
-  resource :session, only: [:new, :create, :destroy] do
-    get :index, path: '/'
-    get :logout
-  end
-
-  namespace :admin do
-    resources :users, only: [:new, :create]
-    root 'overview#index'
-  end
-
-  resources :recipes do
-    get 'show_picture', on: :collection
-  end
-  resources :components do
-    resources :ingredients, only: [:new, :create, :edit, :update, :destroy]
-  end
-
-  root 'recipes#index'
-  # root 'sources#list', defaults: { format: 'markdown' }
+  root 'sources#list'
 end
