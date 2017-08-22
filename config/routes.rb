5f64559d2c082bd94010408a2850494a40135eb9
Manuel <m.a.n.u.e.l@posteo.net>
2017-08-22 19:52:25 +0200

    Adjusted routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,34 @@
 Rails.application.routes.draw do
 
-  resources :products
-  resources :sources do
-    get 'list', on: :collection
-  end
+#  resources :sources do
+#    get 'list', on: :collection
+#  end
 
-  resource :session, only: [:new, :create, :destroy] do
-    get :index, path: '/'
-    get :logout
-  end
+#  resource :session, only: [:new, :create, :destroy] do
+#    get :index, path: '/'
+#    get :logout
+#  end
 
-  namespace :admin do
-    resources :users, only: [:new, :create]
-    root 'overview#index'
-  end
+#  namespace :admin do
+#    resources :users, only: [:new, :create]
+#    root 'overview#index'
+#  end
 
-  resources :recipes do
-    get 'show_picture', on: :collection
-  end
-  resources :components do
-    resources :ingredients, only: [:new, :create, :edit, :update, :destroy]
-  end
+#  resources :recipes do
+#    get 'show_picture', on: :collection
+#  end
+#  resources :components do
+#    resources :ingredients, only: [:new, :create, :edit, :update, :destroy]
+#  end
 
-  root 'recipes#index'
-  # root 'sources#list', defaults: { format: 'markdown' }
+#  root 'recipes#index'
+#  root 'sources#list', defaults: { format: 'markdown' }
+
+  get 'new_session' => 'sources#list'
+  get 'products' => 'sources#list'
+  get 'recipes' => 'sources#list'
+  get 'components' => 'sources#list'
+  get 'sources' => 'sources#list'
+
+  root 'sources#list'
 end
